3fabd9f9c8d94daebfa16a9a1515502d403299e0
Giang Streamy <giang.nguyen@streamy.live>
2019-07-23 11:02:21 +0700

    update deployment

--- app/mailers/phu_quoc_shop_house_mailer.rb
+++ app/mailers/phu_quoc_shop_house_mailer.rb
@@ -6,7 +6,7 @@ class PhuQuocShopHouseMailer < ApplicationMailer
     subject = PhuQuocShopHouse::CAMPAGN_NAME
     @user_email = user_email
     @tracking_url_first = "https://www.google-analytics.com/collect?v=1&tid=UA-137402423-1&cid=#{@user_email}&t=event&ec=ShopHouse&ea=open&dp=%2Femail%2Fmarketing&dt=ShopHouse%20Phu%20Quoc"
-    @tracking_url_second = "https://gadetrungvang.com/phu_quoc_email_marketing/track?email=#{@user_email}"
+    @tracking_url_second = "http://ec2-18-139-162-14.ap-southeast-1.compute.amazonaws.com/phu_quoc_email_marketing/track?email=#{@user_email}"
     mail(to: user_email, subject: subject) do |format|
       format.html
       format.text { render plain: text_plan}
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '52.221.203.224', user: 'deploy', roles: %w{app db web}
+server '18.139.162.14', user: 'deploy', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
