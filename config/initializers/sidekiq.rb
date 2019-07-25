2f5d53db42cc44c32f26ab75e401c5ca2a4a27c3
Giang Streamy <giang.nguyen@streamy.live>
2019-07-25 15:05:03 +0700

    config side kiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,3 +7,11 @@ end
 Sidekiq.configure_client do |config|
   config.redis = sidekiq_config
 end
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+    end if forked
+  end
+end
